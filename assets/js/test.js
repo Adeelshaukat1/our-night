20fee4a9bcc6d1060978c2f139180dfc1ad64793
Adeel Shaukat <adeelshaukat38@gmail.com>
2020-02-05 15:17:24 -0500

    Revert "test button for backend"
    This reverts commit 965fd68dc6803939cdf78ae9126f750e28f16de6.

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,19 +2,13 @@ $(document).ready(function(){
 
     $(".cards-for-genre").hide();
 
-    
-    function showMovies () {
+        $("#btnYes").on("click", function(){
         $("#section").css("background-image", "url(assets/images/landscape-1328858_1920.png)")
         $(".title").text("Please select the following genre:");
         $(".yes").hide();
         $(".cards-for-genre").show();
-        $(".is-rounded").attr("src", "https://media.giphy.com/media/OK5LK5zLFfdm/giphy.gif");
-    }
 
-    $("#btnYes").on("click", showMovies)
+    })
 
-    
-
-    
 
 })
