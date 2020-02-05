965fd68dc6803939cdf78ae9126f750e28f16de6
Adeel Shaukat <adeelshaukat38@gmail.com>
2020-02-05 15:11:22 -0500

    test button for backend

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,13 +2,19 @@ $(document).ready(function(){
 
     $(".cards-for-genre").hide();
 
-        $("#btnYes").on("click", function(){
+    
+    function showMovies () {
         $("#section").css("background-image", "url(assets/images/landscape-1328858_1920.png)")
         $(".title").text("Please select the following genre:");
         $(".yes").hide();
         $(".cards-for-genre").show();
+        $(".is-rounded").attr("src", "https://media.giphy.com/media/OK5LK5zLFfdm/giphy.gif");
+    }
 
-    })
+    $("#btnYes").on("click", showMovies)
 
+    
+
+    
 
 })
