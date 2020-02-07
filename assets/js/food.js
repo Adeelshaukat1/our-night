6b0190bce44dd062ecb5117f90d6e466caa33f38
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-02-07 13:48:37 -0500

    Incorporated AJAX call into index

--- assets/js/food.js
+++ assets/js/food.js
@@ -1,104 +1,95 @@
+$(".food-button").on("click", function(){
+    $(".title").hide();
+    $(".food-button").hide();
+    showFoodOptions();
+})
 
-    // Click event to start ajax call
-    $("#confirmButton").click(function(event) {
-        event.preventDefault();
-    
-        // Store information
-        var cuisine = $("#cuisine").val();
-        var diet = $("#diet").val();
-    
-        // Information for URLs
-        var apiKey = "487918d7ad3042b090e2c9dec14801c7";
-        var queryURL = "https://api.spoonacular.com/recipes/complexSearch?cuisine=" + cuisine + "&diet=" + diet + "&apiKey=" + apiKey;
-        console.log(queryURL);
-    
-    
-        // Ajax call for response
-        $.ajax({
-        method: "GET",
-        url: queryURL
-        }).then(function(response) {
-    
-        // Make random index to return something random
-        var randomIndex = Math.floor(Math.random() * response.results.length) + 1;
-    
-        // Store random picture as well as the title
-        var randompicture = response['results'][randomIndex]['title'];
-        var randomImage = response['results'][randomIndex]['image'];
-    
-        console.log("This is the non random response!");
-        console.log(response);
-        console.log(randompicture);
-        console.log(randomImage);
-        console.log('-------------------------------------------');
-    
-        // Create new images for the received recipies
-        var newImage = $("<img>");
-        newImage = newImage.attr("src", randomImage);
-        $(".images").append(newImage);
-        })})
-    
-    
-    
-    
-    
-    
-    
-    
-        
-        // NO PREFERENCE AJAX CALL!
-        $("#noPreference").click(function(event) {
-        event.preventDefault();
-        
-        // Add variables to query the search
-        var cuisine = $("#cuisine").val();
-        var diet = $("#diet").val();
-        console.log(cuisine);
-        var tags = cuisine + ',' + diet;
-        console.log(tags)
-        // Get 5 random recipies
-        var noPreferenceURL = "https://api.spoonacular.com/recipes/random?number=5&tags=" + tags + "&apiKey=487918d7ad3042b090e2c9dec14801c7";
+
+function showFoodOptions(){
+    $(".cards-for-cuisines").show();
+}
+
+var foodType = '';
+
+ $(".foodCuisine").on("click",function(event){
+    event.preventDefault();
+    var foodType = $(this).attr("data-foodCuisine")
+    var foodTypeEl = document.querySelector(".cards-for-cuisines")
+    foodTypeEl.setAttribute("style", "display: none");
+    $(".cards-for-diet").show();
+    console.log(foodType);
+    return foodType;
+  })
+
+
+  $(".foodDiet").on("click",function(event){
+    event.preventDefault();
+    var foodDiet = $(this).attr("data-foodDiet")
+    console.log(foodDiet)
+    var foodDietEl = document.querySelector(".cards-for-diets")
+    foodDietEl.setAttribute("style", "display: none");
+    // $(".cards-for-diet").show();
+    var tags = foodType + ',' + foodDiet;
+    // Get 5 random recipies
+    var noPreferenceURL = "https://api.spoonacular.com/recipes/random?number=5&tags=" + tags + "&apiKey=487918d7ad3042b090e2c9dec14801c7";
     
         $.ajax({
         method: "GET",
         url: noPreferenceURL    
         }).then(function(response) {
-        console.log("This is the response object!!");
-        console.log(response);
-        console.log("----------------------")
     
         for (var i = 0; i < response['recipes'].length; i++) {
             var image = response['recipes'][i].image;
             var sourceURL = response['recipes'][i].sourceUrl;
             var titleText = response['recipes'][i].title;
-            console.log("Here is food #" + i + 1);
-            console.log("DIET:")
-            console.log(response['recipes'][i]['diets']);
-            console.log("CUISINE:")
-            console.log(response['recipes'][i]['cuisines']);
-            console.log("----------------------------")
+            console.log(image);
+            console.log(sourceURL);
+            console.log(titleText);
+
             var title = $("<h1>");
             title = title.text(titleText);
-            $(".images").append(title);
     
             var a = $("<a>");
                 a = a.attr("href", sourceURL);
                 a = a.attr("target", "_blank");
-                $(".images").append(a);
+
     
             
             var newImage = $("<img>");
                 newImage = newImage.attr("src", image);
-                a.append(newImage);
-        }
+   
+  }})
+
+
+
+$("#confirmButton").click(function(event) {
     
-    
-        
-        
-        })
-        })
-    
-    
-    
-    
-    
+    event.preventDefault();
+
+
+    var intolerances = $("#intolerance").val();
+    var cuisine = $("#cuisine").val();
+    var diet = $("#diet").val();
+    var apiKey = "8d664649a43e494e83023929c061365f";
+    var randomURL = "https://api.spoonacular.com/recipes/random?" + "&apiKey=" + apiKey;
+    var queryURL = "https://api.spoonacular.com/recipes/complexSearch?cuisine=" + cuisine + "&diet=" + diet + "&intolerances=" + intolerances + "&apiKey=" + apiKey;
+
+    $.ajax({
+    method: "GET",
+    url: queryURL
+    }).then(function(response) {
+    console.log("This is the non random response!");
+    console.log(response);
+
+
+    $.ajax({
+        method: "GET",
+        url: randomURL
+    }).then(function(randomResponse){
+    console.log("This is the random response!");    
+    console.log(randomResponse)
+    })
+    });
+
+    })
+  })
--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,11 +1,129 @@
 $(document).ready(function(){
+    $(".cards-for-cuisines").hide();
+    $(".cards-for-genre").hide();
+    $(".cards-for-rating").hide();
+    $(".food-question").hide();
+    $(".cards-for-diet").hide();
 
-
-    $("#btnYes").on("click", function(){
-        $(".hero-is-success").css("background-image", "url()")
-        $(".title").text("Please select the following options:");
-
-    })
-
+    function showHide(){
+        $("#section").css("background-image", "url(assets/images/landscape-1328858_1920.png)")
+        $(".title").text("Please select the following genre:");
+        $(".yes").hide();
+        $(".cards-for-genre").show();
+        $(".is-rounded").hide();
+        $(".cards-for-rating").hide();
+    }
+    $(".yes").on("click", showHide);
 
 })
+
+//when user clicks genre run this function
+var movieID = ""
+
+$(".movieType").on("click",function(event){
+    event.preventDefault();
+    $("#section").css("background-image", "url(assets/images/landscape-1328858_1920.png)")
+    $(".cards-for-rating").show();
+    $(".title").text("Please select the rating: ")
+    movieID = $(this).attr("data-movieID")
+    console.log(movieID)
+    var movieCard = document.querySelector(".cards-for-genre")
+    movieCard.setAttribute("style", "display: none");
+    return (movieID)
+  })
+  $('.dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+});
+$('.dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+
+
+
+});
+/*start the selection of rating*/
+
+console.log(movieID)
+$(".movieCert").on("click",function(event,){
+    $(".food-question").show();
+    event.preventDefault();
+    
+    $("#section").css("background-image", "url()")
+    $(".title").text("Are you interested in making a delicious dinner tonight?")
+
+    var movieCert = $(this).attr("data-movieCert")
+    var movieCertEl = document.querySelector(".cards-for-rating")
+    movieCertEl.setAttribute("style", "display: none");
+    console.log(movieCert);
+    console.log(movieID)
+    var genreURL = "&with_genres="+movieID;
+    var certURL = "&certification_country=US&certification="+movieCert;
+    var movieURL = "https://api.themoviedb.org/3/discover/movie?api_key=f6d55af2d8f9fea21ad4afda60e788bf&language=en-US"+certURL+genreURL+"&region=US&include_adult=false&include_video=true&page=1"
+    console.log(movieURL)
+    $.ajax({
+        url: movieURL,
+        method: "GET",
+      }).then(function(response){
+        console.log(response)
+        var titleCount = response.results.length;
+        var moviesPicked = uniqueFive(titleCount)
+        console.log(moviesPicked)
+        posterImage(moviesPicked, response)
+      })
+})
+
+function createRandom(n){
+    return Math.floor(Math.random()*n)
+  }
+  
+  function uniqueFive(array){
+    var numArr = []
+    var movieShow = []
+    var maxNum = 5
+    for(i = 0; i < array; i++){
+      numArr.push(i)
+    }
+    console.log(numArr)
+    for(i = 0; i < numArr.length; i++){
+      var movieSel = createRandom(numArr.length)
+      var k = 0 
+      while (movieShow.length < maxNum){
+        if (movieSel === movieShow[k]){
+          movieSel = createRandom(numArr.length);
+          console.log(movieSel);
+          k = 0
+        }
+        else if (k === movieShow.length){
+          movieShow.push(movieSel);
+          break;
+        }
+        else{
+          k++;
+        }
+      }
+    }
+    return movieShow;
+  }
+  
+  function posterImage(movie, sel){
+    for(i=0; i <movie.length; i++){
+      var select = movie[i];
+      var picURL = sel.results[select].poster_path
+      var posterURL = "http://image.tmdb.org/t/p/w154/"+picURL
+      console.log(posterURL)
+      var movieImg = $("<img>")
+      movieImg.attr("src", posterURL)
+    }
+  }
+
+
+
+
+
+
